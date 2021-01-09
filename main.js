33dcad1132219ebfc51899661d9c1d0dea471686
BeWiBu <bernd.wissler@kbr.de>
2021-01-09 19:10:41 +0100

    Adding detecting Station catalog, Element definitions

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
+const mosmix = require('./mosmix');
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
@@ -34,11 +35,11 @@ class Template extends utils.Adapter {
     async onReady() {
         // Initialize your adapter here
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        this.config = Object.assign({
+            stationID:''
+        }, this.config);
 
+        
         /*
         For every state in the system there has to be also an object of type state
         Here a simple template for a boolean variable named "testVariable"
@@ -56,33 +57,33 @@ class Template extends utils.Adapter {
             native: {},
         });
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
+        // // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+        // this.subscribeStates('testVariable');
+        // // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+        // // this.subscribeStates('lights.*');
+        // // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
+        // // this.subscribeStates('*');
 
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
+        // /*
+        //     setState examples
+        //     you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
+        // */
+        // // the variable testVariable is set to true as command (ack=false)
+        // await this.setStateAsync('testVariable', true);
 
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
+        // // same thing, but the value is flagged "ack"
+        // // ack should be always set to true if the value is received from or acknowledged from the target system
+        // await this.setStateAsync('testVariable', { val: true, ack: true });
 
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+        // // same thing, but the state is deleted after 30s (getState will return null afterwards)
+        // await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
 
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
+        // // examples for the checkPassword/checkGroup functions
+        // let result = await this.checkPasswordAsync('admin', 'iobroker');
+        // this.log.info('check user admin pw iobroker: ' + result);
 
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        // result = await this.checkGroupAsync('admin', 'admin');
+        // this.log.info('check group user admin group admin: ' + result);
     }
 
     /**
