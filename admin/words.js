0fca91becbde1c1ae0cc5ace4391f9077db5bdec
BeWiBu <bernd.wissler@kbr.de>
2021-01-11 00:12:36 +0100

    Object types changed

--- admin/words.js
+++ admin/words.js
@@ -14,16 +14,16 @@ systemDictionary = {
         'pl': 'Identyfikator stacji',
         'zh-cn': '站号'
     },
-    'View station catalog': {
-        'en': 'View station catalog',
-        'de': 'Stationskatalog anzeigen',
-        'ru': 'Посмотреть каталог станций',
-        'pt': 'Ver catálogo da estação',
-        'nl': 'Bekijk de stationscatalogus',
-        'fr': 'Voir le catalogue de la station',
-        'it': 'Visualizza il catalogo delle stazioni',
-        'es': 'Ver catálogo de estaciones',
-        'pl': 'Zobacz katalog stacji',
-        'zh-cn': '查看电台目录'
+    'Get your Station-ID from the station catalog': {
+        'en': 'Get your Station-ID from the station catalog',
+        'de': 'Holen Sie sich Ihre Stations-ID aus dem Stationskatalog',
+        'ru': 'Получите ваш Station-ID из каталога станций',
+        'pt': 'Obtenha seu Station-ID no catálogo de estações',
+        'nl': 'Haal uw stations-ID op uit de stationscatalogus',
+        'fr': 'Obtenez votre Station-ID dans le catalogue de la station',
+        'it': 'Ottieni il tuo Station-ID dal catalogo delle stazioni',
+        'es': 'Obtenga su ID de estación del catálogo de estaciones',
+        'pl': 'Uzyskaj identyfikator stacji z katalogu stacji',
+        'zh-cn': '从电台目录中获取您的电台ID'
     }
 };
